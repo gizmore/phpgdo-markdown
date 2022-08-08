727ab39280a24f0302773e36ca11b75d9358e677
gizmore <gizmore@wechall.net>
2022-08-08 12:25:05 +0200

    '7.0.1-r1409'

--- js/gdo-markdown.js
+++ js/gdo-markdown.js
@@ -1,24 +1,27 @@
 "use strict";
-
 $(function() {
-	$('.wysiwyg textarea').each(function(){
-		const id = this.id;
-		console.log('Apply markdown editor to #' + id);
-		editormd(id, {
-			name: id + "-md-editor",
-			classPrefix: id + '-mde',
-			path: GDO_WEB_ROOT + 'GDO/Markdown/bower_components/editor.md/lib/',
-			placeholder: '',
-			autoFocus: false,
-			toolbarIcons: [
-	            "undo", "redo", "|", 
-    	        "bold", "italic", "quote", "|", 
-	            "h1", "h2", "h3", "h4", "|", 
-            	"hr", "|",
-            	"link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "emoji", "html-entities", "pagebreak", "|",
-            	"watch", "preview", "fullscreen", "clear",
-			],
-			autoHeight: true,
-		});
-	}); 
+	$('.wysiwyg').each(function() {
+		var id = this.id;
+		setTimeout(function(){
+			if (id) {
+				editormd(id, {
+					name: id + "-md-editor",
+					classPrefix: id + '-mde',
+					path: GDO_WEB_ROOT + 'GDO/Markdown/bower_components/editor.md/lib/',
+					placeholder: '',
+					autoFocus: false,
+					toolbarIcons: [
+			            "undo", "redo", "|", 
+		    	        "bold", "italic", "quote", "|", 
+			            "h1", "h2", "h3", "h4", "|", 
+		            	"hr", "|",
+		            	"link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "emoji", "html-entities", "pagebreak", "|",
+		            	"watch", "preview", "fullscreen", "clear",
+					],
+					autoHeight: true,
+				});
+			}
+		}, 25);
+	});
+	GDO.triggerResize();
 });

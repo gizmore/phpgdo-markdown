e80615ae1705af46bd9b5ca3575388bd501ea33f
gizmore <gizmore@wechall.net>
2022-08-28 06:19:58 +0200

    '7.0.1-r1548'

--- Test/MarkdownTest.php
+++ Test/MarkdownTest.php
@@ -2,16 +2,16 @@
 namespace GDO\Markdown\Test;
 
 use GDO\Tests\TestCase;
-use GDO\Markdown\Decoder;
 use function PHPUnit\Framework\assertEquals;
+use GDO\Markdown\Module_Markdown;
 
 final class MarkdownTest extends TestCase
 {
     public function testHTMLEscape()
     {
         $input = '<hi>';
-        $msg = new Decoder($input);
-        assertEquals('&lt;hi&gt;', $msg->decoded(), 'Test if input validation works for Markdown renderer.');
+        $decoded = Module_Markdown::decode($input);
+        assertEquals('&lt;hi&gt;', $decoded, 'Test if input validation works for Markdown renderer.');
     }
     
 }

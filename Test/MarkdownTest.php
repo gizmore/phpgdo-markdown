f3edee5e0d781308e6c3abb8a645f42e552bdb2d
gizmore <gizmore@wechall.net>
2023-01-30 16:34:37 +0100

    '7.0.1-r1721'

--- Test/MarkdownTest.php
+++ Test/MarkdownTest.php
@@ -10,14 +10,14 @@ final class MarkdownTest extends TestCase
     public function testHTMLEscape()
     {
         $input = '<hi>';
-        $decoded = Module_Markdown::decode($input);
+        $decoded = trim(Module_Markdown::decode($input));
         assertEquals('', $decoded, 'Test if input validation works for Markdown renderer.');
     }
     
     public function testMarkdown()
     {
     	$input = '# Hello';
-    	$decoded = Module_Markdown::decode($input);
+    	$decoded = trim(Module_Markdown::decode($input));
     	assertEquals('<h1>Hello</h1>', $decoded, 'Test roughly if Markdown decoding works.');
     }
     

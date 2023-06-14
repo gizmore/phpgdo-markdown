7d2493ef2dc8ad7581b99de6e731c6336a8c8943
gizmore <gizmore@wechall.net>
2023-06-14 18:11:03 +0200

    '7.0.3-r1794'

---
+++ MethodMarkdown.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GDO\Markdown;
+
+use GDO\UI\MethodPage;
+
+/**
+ * @TODO Renders the page/method.md file as HTML on server side.
+ * Is cached via MethodPage.
+ *
+ * @see MethodPage
+ */
+class MethodMarkdown extends MethodPage
+{
+
+}
